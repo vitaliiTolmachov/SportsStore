92ddd5809afc58d4d0d0f9248521186c827a2601
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-01-29 21:43:35 +0200

    Add RouteConfig + standart View

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Domain.Abstract;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _repository;
+
+        public ProductController(IProductRepository paramRepository)
+        {
+            this._repository = paramRepository;
+        }
+        // GET: Product
+        public ViewResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
