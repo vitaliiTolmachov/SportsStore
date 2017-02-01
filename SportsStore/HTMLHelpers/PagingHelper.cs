e35342d3854e711d74848d4b69cdd3eec820a8dc
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-02-01 23:01:05 +0200

    Добавил HTML-хелпер для формирования ссылок на текущую страницу пагинации

---
+++ SportsStore/HTMLHelpers/PagingHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.HTMLHelpers
+{
+	public static class PagingHelper
+	{
+		static MvcHtmlString PageLinks(this HtmlHelper htmlHelper,
+										PagingInfo pagingInfo,
+										Func<int, string> pageUrlFunc) {
+			var sb = new StringBuilder();
+			for (int i = 0; i < pagingInfo.TotalPages; i++) {
+				var tag = new TagBuilder("a");
+				tag.MergeAttribute("href", pageUrlFunc.Invoke(i));
+				tag.InnerHtml = string.Format("{0} ", i);
+				//mark current page
+				if (i.Equals(pagingInfo.CurrentPage)) {
+					tag.AddCssClass("selected");
+					tag.AddCssClass("btn-primary");
+				}
+				tag.AddCssClass("btn btn-default");
+				sb.Append(tag);
+			}
+			return MvcHtmlString.Create(sb.ToString());
+		}
+	}
+}
---
+++ SportsStore/Models/PagingInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SportsStore.Models
+{
+	public class PagingInfo
+	{
+		public int ProductsCount { get; set; }
+		public int ProductsPerPage { get; set; }
+		public int CurrentPage { get; set; }
+
+		public int TotalPages => (int) Math.Ceiling((decimal) ProductsCount/ProductsPerPage);
+	}
+}
