63c8fb7a2d4098a86bf8d763ae521c6db97561ae
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-02-01 22:35:01 +0200

    Добавил возможность дробление товаров постранично

--- SportsStore.UnitTests/UnitTest1.cs
+++ SportsStore.UnitTests/UnitTest1.cs
@@ -1,13 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using SportsStore.Controllers;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
 
 namespace SportsStore.UnitTests
 {
-    [TestClass]
-    public class UnitTest1
-    {
-        [TestMethod]
-        public void TestMethod1() {
-        }
-    }
+	[TestClass]
+	public class UnitTest1
+	{
+		[TestMethod]
+		public void CanPaginate() {
+			//Arrange
+			var mock = new Mock<IProductRepository>();
+			mock.Setup(repo => repo.Products).Returns(new[]
+			{
+				new Product {ProductId = 1,Name = "Product1"},
+				new Product {ProductId = 2,Name = "Product2"},
+				new Product {ProductId = 3,Name = "Product3"},
+				new Product {ProductId = 4,Name = "Product4"},
+				new Product {ProductId =5, Name = "Product5"}
+			});
+			//Act
+			var controller = new ProductController(mock.Object) { ProductsPerPage = 3 };
+			var result = (IEnumerable<Product>)controller.List(currentPage: 2).Model;
+
+			//Assert
+			var productsFromPage2 = result.ToArray();
+			Assert.AreEqual(2, productsFromPage2.Length);
+			Assert.IsTrue(productsFromPage2[0].ProductId == 4);
+			Assert.IsTrue(productsFromPage2[1].ProductId == 5);
+		}
+	}
 }
--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -7,18 +7,21 @@ using SportsStore.Domain.Abstract;
 
 namespace SportsStore.Controllers
 {
-    public class ProductController : Controller
-    {
-        private IProductRepository _repository;
+	public class ProductController : Controller
+	{
+		private IProductRepository _repository;
 
-        public ProductController(IProductRepository paramRepository)
-        {
-            this._repository = paramRepository;
-        }
-        // GET: Product
-        public ViewResult List()
-        {
-            return View(_repository.Products);
-        }
-    }
+		public int ProductsPerPage = 4;
+		public ProductController(IProductRepository paramRepository) {
+			this._repository = paramRepository;
+		}
+		// GET: Product
+		public ViewResult List(int currentPage = 1)
+		{
+			return View(_repository.Products
+				.OrderBy(p=>p.ProductId)
+				.Skip((currentPage - 1)*this.ProductsPerPage)
+				.Take(ProductsPerPage));
+		}
+	}
 }
