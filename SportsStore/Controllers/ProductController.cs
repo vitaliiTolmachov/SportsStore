83539dbf6b1b4804a5fa0edfdc0d508c1aed316e
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-02-07 22:08:07 +0200

    Added filter by category

--- SportsStore/Controllers/ProductController.cs
+++ SportsStore/Controllers/ProductController.cs
@@ -17,20 +17,19 @@ namespace SportsStore.Controllers
 			this._repository = paramRepository;
 		}
 		// GET: Product
-		public ViewResult List(int currentPage = 1)
-		{
-			var model = new ProducListViewModel
-			{
-				PagingInfo = new PagingInfo
-				{
+		public ViewResult List(string category, int currentPage = 1) {
+			var model = new ProducListViewModel {
+				PagingInfo = new PagingInfo {
 					CurrentPage = currentPage,
 					ProductsPerPage = this.ProductsPerPage,
 					ProductsCount = this._repository.Products.Count()
 				},
 				Products = _repository.Products
+					.Where(p => p.Category == null || p.Category.Equals(category))
 					.OrderBy(p => p.ProductId)
-					.Skip((currentPage - 1)*this.ProductsPerPage)
-					.Take(ProductsPerPage)
+					.Skip((currentPage - 1) * this.ProductsPerPage)
+					.Take(ProductsPerPage),
+				Category = category
 			};
 			return View(model);
 		}
--- SportsStore/Models/ProducListViewModel.cs
+++ SportsStore/Models/ProducListViewModel.cs
@@ -10,5 +10,6 @@ namespace SportsStore.Models
 	{
 		public PagingInfo PagingInfo { get; set; }
 		public IEnumerable<Product> Products { get; set; }
+		public string Category { get; set; }
 	}
 }
