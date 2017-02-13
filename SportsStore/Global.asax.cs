1e9e9101afaf9c36edcbefaa7d520234e569f3fa
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-02-13 23:00:36 +0200

    Register custom binder in app

--- SportsStore/Global.asax.cs
+++ SportsStore/Global.asax.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
+using SportsStore.Domain.Entities;
+using SportsStore.Infrastructure.Binders;
 
 namespace SportsStore
 {
@@ -13,6 +15,7 @@ namespace SportsStore
         {
             AreaRegistration.RegisterAllAreas();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
+            ModelBinders.Binders.Add(typeof(Cart), new CartModelBinder());
         }
     }
 }
