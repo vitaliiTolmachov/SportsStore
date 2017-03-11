8949975aec2f9c95d03d17d22700e76106295d01
Vitalii Tolmachov <v.tolmachov@gmail.com>
2017-03-11 20:57:57 +0200

    Add UnitTest for Model Editor

---
+++ SportsStore.UnitTests/Admin.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Moq.Language.Flow;
+using SportsStore.Controllers;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+
+namespace SportsStore.UnitTests
+{
+	[TestClass]
+	public class Admin
+	{
+		[TestMethod]
+		public void CanEditProduct() {
+			//Arrange
+			var mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products)
+				.Returns(new Product[]
+				{
+					new Product {ProductId = 1, Name = "P1"},
+					new Product {ProductId = 2, Name = "P2"},
+					new Product {ProductId = 3, Name = "P3"}
+				});
+			AdminController target = new AdminController(mock.Object);
+
+			//Act
+			var p1 = target.Edit(1).ViewData.Model as Product;
+			var p2 = target.Edit(2).ViewData.Model as Product;
+			var p3 = target.Edit(3).ViewData.Model as Product;
+
+			//Assert
+			Assert.AreEqual(1, p1.ProductId);
+			Assert.AreEqual(2, p2.ProductId);
+			Assert.AreEqual(3, p3.ProductId);
+		}
+		[TestMethod]
+		public void CanEdit_NoExistentProduct() {
+			//Arrage
+			var mock = new Mock<IProductRepository>();
+			mock.Setup(m => m.Products).Returns(new[]
+			{
+				new Product {ProductId = 1, Name = "P1"},
+				new Product {ProductId = 2, Name = "P2"},
+				new Product {ProductId = 3, Name = "P3"}
+			});
+
+			AdminController target = new AdminController(mock.Object);
+			//Act
+			var product = target.Edit(4).ViewData.Model as Product;
+			//Assert
+			Assert.IsNull(product);
+		}
+	}
+}
--- Test/Entities/Product.cs
+++ Test/Entities/Product.cs
@@ -1,11 +1,15 @@
-﻿namespace SportsStore.Domain.Entities
+﻿using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace SportsStore.Domain.Entities
 {
 	public class Product
 	{
+		[HiddenInput(DisplayValue = false)]
+		public int ProductId{get; set;}
 		public string Name { get; set; }
-
-		public int ProductId { get; set; }
 		public string Description { get; set; }
+		[DataType(DataType.MultilineText)]
 		public string Category { get; set; }
 		public decimal Price { get; set; }
 	}
